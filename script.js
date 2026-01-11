da2f668dec3f3dc81ba88b10e8bd434289b9983c
DpmUnriyo2026 <dpmunriyo26respati@gmail.com>
2026-01-11 14:39:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,6 @@ function toggleTimeline(id) {
 }
 function toggleDesc(card) {
   const desc = card.querySelector('.komisi-desc');
-  const isVisible = window.getComputedStyle(desc).display !== 'none';
-  desc.style.display = isVisible ? 'none' : 'block';
-}
-
-function toggleDesc(card) {
-  const desc = card.querySelector('.komisi-desc');
   desc.classList.toggle('show');
 }
 
